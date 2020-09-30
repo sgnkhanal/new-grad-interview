d15374c2598c8b9c2e8db6d3d3d7aa48824ae620
Sagun Khanal <sgnkhanal@gmail.com>
2020-09-30 14:16:24 -0500

    Create ConvertNumberToWord.java

---
+++ ConvertNumberToWord.java
@@ -0,0 +1,12 @@
+public class ConvertNumberToWord {
+
+  public static void main (String args[]) {
+  
+    //create a map or an array with all the possible words- one, two..ten, twenty and so on
+    
+    //divide the number into digits
+    
+    //map the digits to the map
+    
+
+}
